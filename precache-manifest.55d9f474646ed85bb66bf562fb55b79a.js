d8fcdcfac4bb660900df89de02994f366d20eab7
hopper01 <anand.dhawan01@gmail.com>
2020-08-27 00:29:44 +0530

    Updates

---
+++ precache-manifest.55d9f474646ed85bb66bf562fb55b79a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09666b2d7a88906234f6a9679dd1d6f7",
+    "url": "/morse_encoder/index.html"
+  },
+  {
+    "revision": "04918a9d583d0dfd3cca",
+    "url": "/morse_encoder/static/css/main.91c07d02.chunk.css"
+  },
+  {
+    "revision": "f6ebf9a2955edad09ec7",
+    "url": "/morse_encoder/static/js/2.4e975ea0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/morse_encoder/static/js/2.4e975ea0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04918a9d583d0dfd3cca",
+    "url": "/morse_encoder/static/js/main.7dbbb137.chunk.js"
+  },
+  {
+    "revision": "8f3519b107563e560b0a",
+    "url": "/morse_encoder/static/js/runtime-main.7f83b395.js"
+  }
+]);
